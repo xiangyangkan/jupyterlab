8803bf32d5ace9e5c70469d7fe284cad3efd7ff6
xiangyangkan <xiangyangkan@gmail.com>
2024-12-18 08:36:59 +0800

    refactor: docker multi-stage build

--- docker/build.sh
+++ docker/build.sh
@@ -10,9 +10,10 @@ function docker_prune() {
 
 function build_substreams_image() {
     local python_version="$1"
-    local cmake_version="$2"
-    local buf_version="$3"
-    local substreams_version="$4"
+    local nodejs_version="$2"
+    local cmake_version="$3"
+    local buf_version="$4"
+    local substreams_version="$5"
     local base_image="ubuntu:24.04"
     local stage_1_image="substreams:base"
     local stage_2_image="substreams:conda"
@@ -22,18 +23,19 @@ function build_substreams_image() {
       -t $stage_1_image -f Dockerfile . || exit 1
     docker build --target conda --build-arg BASE_IMAGE="$stage_1_image" --build-arg PYTHON_VERSION="$python_version" \
       -t $stage_2_image -f Dockerfile . || exit 1
-    docker build --target build --build-arg BASE_IMAGE="$stage_2_image" \
-      --build-arg CMAKE_VERSION="$cmake_version" --build-arg BUF_VERSION="$buf_version" \
+    docker build --target devel --build-arg BASE_IMAGE="$stage_2_image" --build-arg PYTHON_VERSION="$python_version" \
       -t $stage_3_image -f Dockerfile . || exit 1
-    docker build --target devel --build-arg BASE_IMAGE="$stage_3_image" --build-arg PYTHON_VERSION="$python_version" \
+    docker build --target build --build-arg BASE_IMAGE="$stage_3_image" --build-arg NODEJS_VERSION="$nodejs_version" \
+      --build-arg CMAKE_VERSION="$cmake_version" --build-arg BUF_VERSION="$buf_version" \
       -t $final_image -f Dockerfile . || exit 1
     docker push $final_image && docker_prune
 }
 
 PYTHON_VERSION="3.11"
+NODEJS_VERSION="23.4.0"
 CMAKE_VERSION="3.30.6"
 BUF_VERSION="1.47.2"
 SUBSTREAMS_VERSION="1.11.3"
 
 dos2unix ./*
-build_substreams_image "$PYTHON_VERSION" "$CMAKE_VERSION" "$BUF_VERSION" "$SUBSTREAMS_VERSION"
+build_substreams_image "$PYTHON_VERSION" "$NODEJS_VERSION" "$CMAKE_VERSION" "$BUF_VERSION" "$SUBSTREAMS_VERSION"
